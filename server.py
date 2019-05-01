0e053a8e70f3154e807ea41227fd8ffb70b58bc3
Unknown <torres-jacobo1@hotmail.com>
2019-04-30 23:48:27 -0500

    Primer paso

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+   return render_template('view1.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template('view2.html',result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
